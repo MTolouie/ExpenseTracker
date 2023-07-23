d4fc8cd5233a33bf83560ec8626600911ff52676
amirmtolouie@gmail.com <amirmtolouie@gmail.com>
2023-07-23 19:12:52 +0300

    Card Component Added

--- src/Components/Expenses.js
+++ src/Components/Expenses.js
@@ -1,12 +1,13 @@
+import Card from "./Card";
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css"
 function Expenses(props) {
   return (
-    <div className="expenses">
+    <Card className="expenses">
       <ExpenseItem expense={props.expenses[0]} />
       <ExpenseItem expense={props.expenses[1]} />
       <ExpenseItem expense={props.expenses[2]} />
-    </div>
+    </Card>
   );
 }
 
